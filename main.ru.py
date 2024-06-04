5daa9af63072d2540318ed872cb8151f6f73709a
Lufanin <orion.pax37@gmail.com>
2024-06-04 16:47:56 +0300

    Add files via upload

---
+++ main.ru.py
@@ -0,0 +1,29 @@
+print('a')
+print('Длина строки:', len('a'))
+print()
+print()
+first= 5
+second= 4
+third= 3
+print(first + second)
+print(first - second)
+summa=(first + second)
+diff=(first-second)
+print()
+print()
+print('Сумма:', summa)
+print('Разница:',diff)
+mean=((first + second + third) // (3))
+print(mean)
+print()
+print()
+first_string= 'вторник'
+second_string= 'понедельник'
+print(second_string + ', ' + first_string)
+print()
+print()
+a=1
+b=2
+c=3
+f=(a * b) + (a * c)
+print(f**3/2)
