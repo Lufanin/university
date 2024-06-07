4b13a126ba7794527206f637c2bff2dc44837516
Lufanin <orion.pax37@gmail.com>
2024-06-07 12:26:58 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+exemple='Топинамбур'
+print(len(exemple)) # это для себя, поможет узнать сколько символов в слове
+
+print(exemple[0]) # задание №1
+
+print(exemple[-1]) # задание №2
+
+print(exemple[5:]) # Задание №3
+
+print(exemple[::-1]) # Задание №4
+
+print(exemple[1:10:2]) # Задание №5
