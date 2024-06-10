c8c952feff108c4e5b11b20cbae8812850f490f0
Lufanin <orion.pax37@gmail.com>
2024-06-10 18:59:49 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -4,3 +4,5 @@ print('immutable_var: ' + str(immutable_var))
 #immutable_var[2]= False (TypeError: 'tuple' object does not support item assignment)
 mutable_list=[2.2,False,'Say', 300]
 print('mutable_list: ' + str(mutable_list))
+mutable_list[2]="Just"
+print('mutable_list: ' + str(mutable_list))
