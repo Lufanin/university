2e4759a396b087dad8fc37e7d7801d956d2770b5
Lufanin <orion.pax37@gmail.com>
2024-06-10 15:49:12 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Дмитрий'
+age = 28
+new_age = age +1
+is_student = True
+print('Name: ' + name)
+print('Age: ' + str(age))
+print('New Age: ' + str(new_age))
+print('Is Student: '+ str(is_student))
