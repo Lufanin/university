7d0c8e459122b48daa56350c6d63d5df740332ce
Lufanin <orion.pax37@gmail.com>
2024-06-11 18:33:51 +0300

    Update main.ru.py

--- main.ru.py
+++ main.ru.py
@@ -1,7 +1,7 @@
 print('a')
-print('Длина строки:', len('a'))
-print()
-print()
+print('Длина строки:', len('a')
+
+      
 first= 5
 second= 4
 third= 3
@@ -9,19 +9,19 @@ print(first + second)
 print(first - second)
 summa=(first + second)
 diff=(first-second)
-print()
-print()
+
+
 print('Сумма:', summa)
 print('Разница:',diff)
 mean=((first + second + third) // (3))
 print(mean)
-print()
-print()
+
+
 first_string= 'вторник'
 second_string= 'понедельник'
 print(second_string + ', ' + first_string)
-print()
-print()
+
+
 a=1
 b=2
 c=3
