96c6818b14a074b9e0ef1b3373e89fe4bc38c659
Lufanin <orion.pax37@gmail.com>
2024-06-13 23:41:26 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -5,9 +5,3 @@ grades_m =[sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grade
            sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])]
 a=dict(zip(students_sort,grades_m))
 print(a)
-
-students_sort = sorted(students)
-grades_m =[sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]),
-           sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])]
-a=dict(zip(students_sort,grades_m))
-print(a)
